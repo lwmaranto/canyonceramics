98027e23363b628530b5cbb92101a36eb7c44f82
lwmaranto <laurawmaranto@gmail.com>
2021-03-08 21:22:28 -0800

    previous

--- server/api/users.js
+++ server/api/users.js
@@ -3,7 +3,7 @@ const {User} = require('../db/models')
 module.exports = router
 
 // SECURE ROUTES
-const forAdmin = (req, res, next) => {
+/*const forAdmin = (req, res, next) => {
   if (!req.user || !req.user.isAdmin) {
     const err = new Error('This page is only available to admins!')
     err.status = 401
@@ -14,6 +14,21 @@ const forAdmin = (req, res, next) => {
 
 router.get('/', forAdmin, async (req, res, next) => {
   try {
+    const users = await User.findAll({
+      // explicitly select only the id and email fields - even though
+      // users' passwords are encrypted, it won't help if we just
+      // send everything to anyone who asks!
+      attributes: ['id', 'email']
+    })
+    res.json(users)
+  } catch (err) {
+    next(err)
+  }
+})*/
+
+router.get('/', async (req, res, next) => {
+  try {
+    console.log(Object.keys(User.prototype))
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
